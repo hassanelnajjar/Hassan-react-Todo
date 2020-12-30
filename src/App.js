b90ccca5a2b4cecfaa940e3b5fd3c9361c6ad80e
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-12-30 23:58:01 +0200

    create dummy data relate #3

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import ReactDOM from 'react-dom';
-import React from 'react';
+import React, { Component } from 'react';
+import data from './Utils/data';
 
-React.createElement('div',{word:'red'},'Hello')
-ReactDOM.render(React.createElement('div',{word:'red'},'Hello'),document.getElementById('expample'))
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+	state = data;
+	render() {
+		return <div>Hello</div>;
+	}
 }
-
-export default App;
