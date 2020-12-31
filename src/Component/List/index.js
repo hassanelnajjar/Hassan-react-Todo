1ad37a47b9c28d95902675cb76df2a7cd5aa7fee
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-12-31 23:49:44 +0200

    added change color to lists

--- src/Component/List/index.js
+++ src/Component/List/index.js
@@ -1,57 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Task from '../Task';
 import AddTask from '../AddTask';
 import './style.css';
-export default function List(props) {
-	const {
-		handleChangeListColor,
-		listName,
-		listId,
-		tasks,
-		methods: { handleDeleteList, handleListNameUpdate, ...methods },
-	} = props;
-	return (
-		<div className='List-div'>
-			<div className='List-header'>
-				<div className='List-AddTask-Button-Div'>
-					<span className='List-plus-symbol'>
-						<i className='fas fa-plus'></i>
-					</span>
-					<button className='List-AddTask-button'>Add a task</button>
-				</div>
+export default class List extends Component {
+	state = {
+		color: '#fff',
+	};
+	changeColor = () => {
+		const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+		this.setState(() => ({ color: `#${randomColor}` }));
+	};
+	render() {
+		const {
+			listName,
+			listId,
+			tasks,
+			methods: { handleDeleteList, handleListNameUpdate, ...methods },
+		} = this.props;
+		const style = {
+			backgroundColor: this.state.color,
+		};
 
-				<input
-					className='List-TaskName-input'
-					type='text'
-					value={listName}
-					onChange={(e) => handleListNameUpdate(listId, e)}
-				/>
-				<button
-					className='List-Change-Color-Button'
-					onClick={handleChangeListColor}
-				>
-					<i className='fas fa-paint-brush'></i>
-				</button>
-				<button
-					className='List-Delete-Button'
-					onClick={(e) => handleDeleteList(listId, e)}
-				>
-					<i className='far fa-trash-alt'></i>
-				</button>
-			</div>
-			<div className='List-Tasks-Container'>
-				<AddTask listId={listId} methods={methods} taskText={'Add Task'} />
-				{tasks.map((task) => (
-					<Task
-						key={task.id}
-						taskText={task.text}
-						taskId={task.id}
-						listId={listId}
-						isCompleted={task.completed}
-						methods={methods}
+		return (
+			<div className='List-div' style={style}>
+				<div className='List-header'>
+					<div className='List-AddTask-Button-Div'>
+						<span className='List-plus-symbol'>
+							<i className='fas fa-plus'></i>
+						</span>
+						<button className='List-AddTask-button'>Add a task</button>
+					</div>
+
+					<input
+						className='List-TaskName-input'
+						type='text'
+						value={listName}
+						onChange={(e) => handleListNameUpdate(listId, e)}
 					/>
-				))}
+					<button
+						className='List-Change-Color-Button'
+						onClick={this.changeColor}
+					>
+						<i className='fas fa-paint-brush'></i>
+					</button>
+					<button
+						className='List-Delete-Button'
+						onClick={(e) => handleDeleteList(listId, e)}
+					>
+						<i className='far fa-trash-alt'></i>
+					</button>
+				</div>
+				<div className='List-Tasks-Container'>
+					<AddTask listId={listId} methods={methods} taskText={'Add Task'} />
+					{tasks.map((task) => (
+						<Task
+							key={task.id}
+							taskText={task.text}
+							taskId={task.id}
+							listId={listId}
+							isCompleted={task.completed}
+							methods={methods}
+						/>
+					))}
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
