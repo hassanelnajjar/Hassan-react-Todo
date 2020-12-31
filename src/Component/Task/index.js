8820f3c0755b1c73ce2be83b5c56ef954c30a900
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-12-31 10:34:36 +0200

    change circle tag from span to button and fix it's styles relates #5

--- src/Component/Task/index.js
+++ src/Component/Task/index.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import './style.css';
 export default function Task(props) {
+	const {
+		handleDeleteTask,
+		handleEditTask,
+		handleCompleted,
+		handleInputTask,
+	} = props;
 	return (
 		<div className='Task-div'>
 			<div className='Task-text-input-div'>
-				<span class='Task-circle'></span>
-				<input className='Task-text-input' type='text' name='' id='' />
+				<button class='Task-circle-button' onClick={handleCompleted}></button>
+				<input
+					className='Task-text-input'
+					type='text'
+					name=''
+					id=''
+					onClick={handleInputTask}
+				/>
 			</div>
 
-			<button className='Task-edit-button'>
+			<button className='Task-edit-button' onClick={handleEditTask}>
 				<i className='fas fa-pencil-alt'></i>
 			</button>
-			<button className='Task-delete-button'>
+			<button className='Task-delete-button' onClick={handleDeleteTask}>
 				<i className='far fa-trash-alt'></i>
 			</button>
 		</div>
