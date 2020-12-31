e49ad88e0d2eb331abeffb2584e04873ba34a1bc
hassanelnajjar <hassansamielnajjar@gmail.com>
2020-12-31 23:37:32 +0200

    fix add list button

--- src/Component/AddButton/index.js
+++ src/Component/AddButton/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './style.css';
 export default function AddButton(props) {
-	console.log(props.handleAddList);
 	return (
 		<button className='AddButton-Class' onClick={props.handleAddList}></button>
 	);
